1c14697a1a37fe1b17f4b0dcc2b2502bb8d0403d
shaune <contact@shaunewest.com>
2016-12-05 23:47:58 +1100

    Add readme

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express')
+  , router  = express.Router()
+  , path    = require('path');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/*?', function(req, res, next) {
+  
+  /* Retrive optional paramater */
+  let user = (path.basename(req.path) ? path.basename(req.path) : 'not set, please try the following url `users/foo/bar/baz`.');
+  
+  /* send a response to the user */
+  res.send('Optional wildcard param is: '+user);
 });
 
 module.exports = router;
